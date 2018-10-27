85e5325e7b42f01f09e3af9eeccf42d3bdea0b62
Mario Matheus <mariomatheus@MacBook-Pro-33.local>
2018-10-27 07:52:50 -0300

    Inicial Commit

---
+++ Pokcatch/Controllers/PokemonMapViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PokemonMapViewController.swift
+//  Pokcatch
+//
+//  Created by Mario Matheus on 27/10/18.
+//  Copyright © 2018 Coding Eagles. All rights reserved.
+//
+
+import UIKit
+
+class PokemonMapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
